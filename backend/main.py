aeab096f8d316de2cd973cde8f2207cd89155219
Weixuan Zhang <weixuanz.git@icloud.com>
2022-01-22 16:00:40 +0000

    Fix main

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,12 @@ import asyncio
 import json
 
 from transcription import transcribe
+from video_processor import Video
 
 
 async def main():
-    transcript = await transcribe("metaverse_short.mp4")
+    video = Video("metaverse_short.mp4")
+    transcript = await transcribe(video.audio)
     print(json.dumps(transcript, indent=4))
     print(type(transcript))
 

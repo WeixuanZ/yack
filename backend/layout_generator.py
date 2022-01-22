38abc5004b3d9193d7346d48fa4ede5d64b3baf5
Thomas Grant <thomas.grant.duck321@gmail.com>
2022-01-22 13:20:41 +0000

    Rough project structure

---
+++ backend/layout_generator.py
@@ -0,0 +1,3 @@
+class LayoutGenerator:
+    def addFrame(frameData, speech_bubble_content):
+        pass
---
+++ backend/structures.py
@@ -0,0 +1,4 @@
+class SpeechContainer:
+    def __init__(self, speech_text, speaker_location = None) -> None:
+        self.speech_text = speech_text
+        self.speaker_location = speaker_location

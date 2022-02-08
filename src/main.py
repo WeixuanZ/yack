c84ddea0c2d62085e978b91b061e62933436c7dd
Tom Grant <thomas.grant.mail@gmail.com>
2022-02-08 00:22:20 +0000

    Revert to default port number

--- src/main.py
+++ src/main.py
@@ -217,4 +217,4 @@ def submit_video_api():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8081, debug=True, threaded=True)
+    app.run(host="127.0.0.1", port=8000, debug=True, threaded=True)
